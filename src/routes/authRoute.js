ad66d41281651431d4ff7707881ef1e42028caf4
Sam <samshrestha85@gmail.com>
2020-09-07 19:43:58 +0545

    Added login protected route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController.js');
+const {
+  signup,
+  login,
+  requireSignIn,
+} = require('../controllers/authController.js');
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+
+router.post('/profile', requireSignIn, (req, res) => {
+  res.status(200).json({ user: 'profile' });
+});
 
 module.exports = router;
