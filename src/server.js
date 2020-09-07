79fa3a3e7a407e4880444d46f72192fbc2706dbd
Sam <samshrestha85@gmail.com>
2020-09-07 18:59:45 +0545

    Added initials,login signup,userModel, errorHandling

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const env = require('dotenv');
+const bodyParser = require('body-parser');
+
+const GlobalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
+
+const userRoute = require('./routes/authRoute');
+
+const app = express();
+
+// environment variable
+env.config();
+
+// mongoose DB
+const db = process.env.MONGO_URI;
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Database connected...');
+  });
+
+// middleware
+app.use(bodyParser.json());
+
+// routes
+app.use('/api/users', userRoute);
+
+// middleware for unknown route to show error for all HTTPHeaders
+// Global error handling Middleware
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(GlobalErrorHandler);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
